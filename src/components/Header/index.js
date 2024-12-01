87a1bdc1e9ef41847ab0ce61df06b188cfa79f71
Luiz <=>
2024-12-01 19:59:21 -0300

    Header da aplicação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Nav } from './style';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 
-
+export default function Header() {
+  return (
+    <Nav>
+      <a href="#">
+        <FaHome size={24} />
+      </a>
+      <a href="#">
+        <FaUserAlt size={24} />
+      </a>
+      <a href="#">
+        <FaSignInAlt size={24} />
+      </a>
+    </Nav>
+  );
+}
